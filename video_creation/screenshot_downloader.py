ae1de3d6eac17fd8b9132c183e57e58d19ae64f0
J Goraj <jckgrj@gmail.com>
2022-06-18 13:12:10 +0200

    wrapped error where entering thread failed

--- video_creation/screenshot_downloader.py
+++ video_creation/screenshot_downloader.py
@@ -35,33 +35,40 @@ def download_screenshots_of_reddit_posts(reddit_object, screenshot_num, theme):
 
         # Get the thread screenshot
         page = context.new_page()
-        page.goto(reddit_object["thread_url"])
-        page.set_viewport_size(ViewportSize(width=1920, height=1080))
-        if page.locator(NSFW_PROMPT).is_visible():
-            # This means the post is NSFW and requires to click the proceed button.
+        erred = False
+        try:
+            page.goto(reddit_object["thread_url"])
+        except Exception as e:
+            print_substep("There was an exception while opening reddit thread. " + str(e))
+            print_substep("Something is not ok, will skip this thread.")
+            erred = True
 
-            print_substep("Post is NSFW. You are spicy...")
-            page.locator(NSFW_PROMPT_ACCEPT_BUTTON).click()
+        if not erred:
+            page.set_viewport_size(ViewportSize(width=1920, height=1080))
+            if page.locator(NSFW_PROMPT).is_visible():
+                # This means the post is NSFW and requires to click the proceed button.
 
-        page.locator(THREAD_TITLE_LABEL).screenshot(
-            path="assets/png/title.png"
-        )
+                print_substep("Post is NSFW. You are spicy...")
+                page.locator(NSFW_PROMPT_ACCEPT_BUTTON).click()
 
-        for idx, comment in track(
-            enumerate(reddit_object["comments"]), "Downloading screenshots..."
-        ):
-
-            # Stop if we have reached the screenshot_num
-            if idx >= screenshot_num:
-                break
-
-            if page.locator('[data-testid="content-gate"]').is_visible():
-                page.locator('[data-testid="content-gate"] button').click()
-
-            page.goto(f'https://reddit.com{comment["comment_url"]}')
-            page.locator(f"#t1_{comment['comment_id']}").screenshot(
-                path=f"assets/png/comment_{idx}.png"
+            page.locator(THREAD_TITLE_LABEL).screenshot(
+                path="assets/png/title.png"
             )
 
-        print_substep("Screenshots downloaded Successfully.",
-                      style="bold green")
+            for idx, comment in track(
+                    enumerate(reddit_object["comments"]), "Downloading screenshots..."
+            ):
+                # Stop if we have reached the screenshot_num
+                if idx >= screenshot_num:
+                    break
+
+                if page.locator('[data-testid="content-gate"]').is_visible():
+                    page.locator('[data-testid="content-gate"] button').click()
+
+                page.goto(f'https://reddit.com{comment["comment_url"]}')
+                page.locator(f"#t1_{comment['comment_id']}").screenshot(
+                    path=f"assets/png/comment_{idx}.png"
+                )
+
+            print_substep("Screenshots downloaded Successfully.",
+                          style="bold green")
